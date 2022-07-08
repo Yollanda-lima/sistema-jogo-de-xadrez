dcfc20175a5d45620b36a59df2c1b2170b33f907
Yollanda Lima <yollandalima123@gmail.com>
2022-07-08 12:42:27 -0300

    Tratamento de exceções e tela de limpeza

--- src/aplicacao/App.java
+++ src/aplicacao/App.java
@@ -1,11 +1,13 @@
 package aplicacao;
 
 import java.awt.Point;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import tabuleiro.Peca;
 import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
+import xadrez.ExcecaoXadrez;
 import xadrez.PartidaDeXadrez;
 import xadrez.PecaDeXadrez;
 import xadrez.PosicaoXadrez;
@@ -18,19 +20,30 @@ public class App {
 		PartidaDeXadrez partidaDeXadrez = new PartidaDeXadrez();
 		
 		while (true) {
-			UI.printTabuleiro(partidaDeXadrez.getpecas());
-			System.out.println();
-			System.out.println("Origem: ");
-			PosicaoXadrez origem = UI.lerPosicaoDeXadrez(sc);
-			
-			System.out.println();
-			System.out.println("Destino: ");
-			PosicaoXadrez destino = UI.lerPosicaoDeXadrez(sc);
-			
-			PecaDeXadrez capurarPeca = partidaDeXadrez.executarMovimentoDeXadrez(origem, destino);
-		}
+			try {
+				UI.clearScreen();
+				UI.printTabuleiro(partidaDeXadrez.getpecas());
+				System.out.println();
+				System.out.println("Origem: ");
+				PosicaoXadrez origem = UI.lerPosicaoDeXadrez(sc);
+				
+				System.out.println();
+				System.out.println("Destino: ");
+				PosicaoXadrez destino = UI.lerPosicaoDeXadrez(sc);
+				
+				PecaDeXadrez capurarPeca = partidaDeXadrez.executarMovimentoDeXadrez(origem, destino);
+			}
+			catch(ExcecaoXadrez e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 	
-
+	
+		}
 	}
 
 }
