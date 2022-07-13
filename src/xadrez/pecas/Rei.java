7d2d4d829e10ea181e85c29daf2fd5d408ef341d
Yollanda Lima <yollandalima123@gmail.com>
2022-07-13 16:45:41 -0300

    Implementando possíveis movimentos do Rei

--- src/xadrez/pecas/Rei.java
+++ src/xadrez/pecas/Rei.java
@@ -1,5 +1,6 @@
 package xadrez.pecas;
 
+import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import xadrez.Cor;
 import xadrez.PecaDeXadrez;
@@ -9,15 +10,55 @@ public class Rei extends PecaDeXadrez {
 	public Rei(Tabuleiro tabuleiro, Cor cor) {
 		super(tabuleiro, cor);
 	}
-	
+
 	@Override
 	public String toString() {
-		return "R"; //R de Rei
+		return "R"; // R de Rei
+	}
+
+	private boolean podeMover(Posicao posicao) {
+		PecaDeXadrez p = (PecaDeXadrez) getTabuleiro().peca(posicao);
+		return p == null || p.getCor() != getCor();
 	}
 
 	@Override
 	public boolean[][] movimentosPossiveis() {
 		boolean[][] matriz = new boolean[getTabuleiro().getRowsLinhas()][getTabuleiro().getColumnColuna()];
+
+		Posicao p = new Posicao(0, 0);
+
+		p.setValores(posicao.getRowFileira() - 1, posicao.getColumnColuna());
+		if (getTabuleiro().posiçãoExiste(p) && podeMover(p)) {
+			matriz[p.getRowFileira()][p.getColumnColuna()] = true;
+		}
+		p.setValores(posicao.getRowFileira() + 1, posicao.getColumnColuna());
+		if (getTabuleiro().posiçãoExiste(p) && podeMover(p)) {
+			matriz[p.getRowFileira()][p.getColumnColuna()] = true;
+		}
+		p.setValores(posicao.getRowFileira(), posicao.getColumnColuna() - 1);
+		if (getTabuleiro().posiçãoExiste(p) && podeMover(p)) {
+			matriz[p.getRowFileira()][p.getColumnColuna()] = true;
+		}
+		p.setValores(posicao.getRowFileira(), posicao.getColumnColuna() + 1);
+		if (getTabuleiro().posiçãoExiste(p) && podeMover(p)) {
+			matriz[p.getRowFileira()][p.getColumnColuna()] = true;
+		}
+		p.setValores(posicao.getRowFileira() - 1, posicao.getColumnColuna() - 1);
+		if (getTabuleiro().posiçãoExiste(p) && podeMover(p)) {
+			matriz[p.getRowFileira()][p.getColumnColuna()] = true;
+		}
+		p.setValores(posicao.getRowFileira() - 1, posicao.getColumnColuna() + 1);
+		if (getTabuleiro().posiçãoExiste(p) && podeMover(p)) {
+			matriz[p.getRowFileira()][p.getColumnColuna()] = true;
+		}
+		p.setValores(posicao.getRowFileira() + 1, posicao.getColumnColuna() - 1);
+		if (getTabuleiro().posiçãoExiste(p) && podeMover(p)) {
+			matriz[p.getRowFileira()][p.getColumnColuna()] = true;
+		}
+		p.setValores(posicao.getRowFileira() + 1, posicao.getColumnColuna() + 1);
+		if (getTabuleiro().posiçãoExiste(p) && podeMover(p)) {
+			matriz[p.getRowFileira()][p.getColumnColuna()] = true;
+		}
 		return matriz;
 	}
 
